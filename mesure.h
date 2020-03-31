ad4b3a7d5c214827fe182527a2598b9790d21f5d
fano <fanoarii.boyer@isen-ouest.yncrea.fr>
2020-03-31 17:30:10 +0200

    avancement mesure

--- mesure.h
+++ mesure.h
@@ -1,10 +1,14 @@
 #include "define.h"
-
-oxy mesureTest(char* filename);
-
+#include "fichiers.h"
 typedef struct{
-
+    float lastMaximum;
+    float lastMinimum;
+    float compteur;
+    float previousVal;
 }param_mesure;
 
-oxy mesure(absorp myAbsorp, param_mesure* myMes);
+param_mesure* init_mesure();
+void fin_mesure(param_mesure* myMes);
+oxy mesure(oxy myOxy,absorp myAbsorp, param_mesure* myMes);
+oxy mesureTest(char* filename);
 
